a7fb3a533355b99e511e2e3b87c6d779590c08d7
congnam1997 <namhung024@gmail.com>
2023-05-17 14:01:19 +0700

    update

--- case_study/case_study_task_6-10.sql
+++ case_study/case_study_task_6-10.sql
@@ -2,7 +2,7 @@ use quan_ly_resort_furama;
 -- Task 6:	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich,chi_phi_thue, ten_loai_dich_vu 
 -- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3)
 select 
-dv.ma_dich_vu,
+distinct dv.ma_dich_vu,
 dv.ten_dich_vu,
 dv.dien_tich,
 dv.chi_phi_thue,
@@ -10,11 +10,13 @@ ldv.ten_loai_dich_vu
 from dich_vu dv 
 join loai_dich_vu ldv
 on dv.ma_loai_dich_vu  = ldv.ma_loai_dich_vu
-where dv.ma_loai_dich_vu not in 
-(select hd.ma_dich_vu 
-from hop_dong hd 
-where (quarter(hd.ngay_lam_hop_dong)=1)
-and (year(hd.ngay_lam_hop_dong)=2021)
+join hop_dong hd 
+on hd.ma_dich_vu = dv.ma_dich_vu
+where hd.ma_dich_vu not in 
+(select ma_dich_vu 
+from hop_dong 
+where (quarter(ngay_lam_hop_dong)=1)
+and (year(ngay_lam_hop_dong)=2021)
 );
 
 -- Task 7:	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu
@@ -57,16 +59,19 @@ select ho_ten
 from khach_hang;
 
 -- Task 9	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
-select month(ngay_lam_hop_dong) as "thang",
+select
+ month(ngay_lam_hop_dong) as "thang",
  count(ma_khach_hang) as "so_luong_khach_hang"
 from hop_dong
+where year(ngay_lam_hop_dong) =2021
 group by month(ngay_lam_hop_dong)
 order by month(ngay_lam_hop_dong);
 
 -- Task 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm.
 --  Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem 
 -- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
-select hd.ma_hop_dong,
+select
+ hd.ma_hop_dong,
  hd.ngay_lam_hop_dong,
  hd.ngay_ket_thuc,
  hd.tien_dat_coc,
